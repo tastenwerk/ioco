00d5887cd996d700c88f1c8a3ce20ba443525236
thorsten zerha <quaqua@tastenwerk.com>
2013-04-18 00:29:51 +0200

    tmpl plugins for web prepared

--- public/javascripts/ioco.kendo-addons.js
+++ public/javascripts/ioco.kendo-addons.js
@@ -11,19 +11,27 @@
  */
 ( function(){
 
-  this.kendo.data.binders.css = kendo.data.Binder.extend( {
-      refresh: function() {
-        if( this.bindings.css ){
-          var path = this.bindings.css.path;
-          for( var i in path ){
-            if( this.bindings.css.source[path[i]]() )
+  this.kendo.data.binders.css = this.kendo.data.Binder.extend( {
+    refresh: function() {
+      if( this.bindings.css ){
+        var path = this.bindings.css.path;
+        for( var i in path ){
+          if( path[i] in this.bindings.css.source ){
+            var res = this.bindings.css.source[path[i]];
+            if( typeof(res) === 'function' )
+              res = res();
+            if( res )
               $(this.element).addClass(i);
             else
               $(this.element).removeClass(i);
+          } else{
+            console.log('error:', path[i], 'is not defined for', this.bindings.css.source );
+            throw new Error(path[i]+' is not defined');
           }
         }
-
       }
+
+    }
   });
 
 })();
