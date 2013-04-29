2114582bc875e6a155302204d10340547a3cc494
thorsten zerha <quaqua@tastenwerk.com>
2013-04-29 02:18:53 +0200

    up

--- public/javascripts/ioco.core.js
+++ public/javascripts/ioco.core.js
@@ -1,7 +1,7 @@
 /**
  * ioco.core.js
  *
- * main features and wrappers for the ioco system
+ * main features and wrappers for the ioco eco system
  *
  * (c) TASTENWERK 2013
  *
@@ -18,7 +18,7 @@
 
   var _required = [];
 
-  // used for global datasources namespace
+  // used for global datasources namespace (knockoutjs or kendoui datasources for example)
   var sources = {};
 
   /**
@@ -45,6 +45,7 @@
     }
   }
 
+
   /**
    * log a string to the console
    *
@@ -66,202 +67,6 @@
     } catch(e){ console.log('logging not supported')}
 
   }
-
-  /**
-   * a window modal
-   *
-   * can be used as a small dialog window
-   * or a large configuration modal
-   *
-   * Examples:
-   *     
-   *     ioco.window({ type: 'dialog', text: 'Are you sure?' })
-   *       .on('submit', function( name ){
-   *         console.log(name);
-   *       });
-   *
-   * @param {Object}
-   * * type: 'dialog', null (dialog creates a preformatted form)
-   * * content: the text for the dialog. can also be a full jQuery object
-   * * submit: function( name )
-   *
-   */
-  function window( options ){
-
-    _defaults = { width: '400px', title: 'Attention required', actions: ['Close'] };
-
-    for( var i in options )
-      _defaults[i] = options[i];
-
-    var $win = $('<div class="ioco-win" />');
-
-    if( options.type && options.type === 'dialog' ){
-      $win.append('<form class="dialog-form"><p><label>'+options.content+'</label><br /><input type="text" name="name" /></p>'+
-                  '<p><a class="submit-btn btn">'+ (options.submitText || 'OK') +'</a></p>')
-        .find('.submit-btn').on('click', function( e ){
-          $(this).closest('form').submit();
-        }).end()
-        .find('form').on('submit', function(e){
-          e.preventDefault();
-          if( typeof(options.submit) === 'function' )
-            options.submit( $(this).find('[name=name]').val() );
-          $win.data("kendoWindow").close();
-        });
-    } else{
-      $win.append( options.content );
-
-      if( $win.find('.side-tabs').length ){
-        $win.find('.side-tabs-nav > li').on('click', function(){
-          $(this).closest('ul').find('.active').removeClass('active');
-          $win.find('.side-tabs-content > div').hide();
-          $($win.find('.side-tabs-content > div')[$(this).index()]).show();
-          $(this).addClass('active');
-        }).first().click();
-      }
-
-    }
-
-    $('body').append( $win );
-    $win.kendoWindow({
-      width: _defaults.width,
-      title: _defaults.title,
-      modal: (options.type && options.type === 'dialog'),
-      center: true,
-      resizeable: false,
-      actions: options.actions,
-      activate: function(){
-        if( options.type && options.type === 'dialog' )
-          $win.find('[name=name]').focus();
-        if( typeof( options.activate ) === 'function' )
-          options.activate( $win );
-        $win.data('kendoWindow').center();
-      },
-      deactivate: function(){
-        if( typeof( options.deactivate ) === 'function' )
-          options.deactivate( $win );
-      }
-    });
-    
-
-  } 
-
-  /**
-  * adds a blocking modal box to the whole
-  * screen
-  *
-  * @param {String} [html] the html string to be rendered to this modal
-  * also, action strings are valid:
-  *
-  * @param {Object} [options] options are:
-  * * height: desired height of modal window
-  * * before: callback function, before modal will be shown
-  * * completed: callback function, after modal has been shown and is visible
-  * * html: the html content (optional can be passed as first argument)
-  * * $content: a dom object to be appended
-  *
-  * to the user.
-  * * url: remote url, if this modal should be loaded from url
-  * * on:
-  *   new callback function. specify exact action by parsing the parameter
-  *   on('close', function( $modal ) )
-  *
-  * @param {Function} [callback] the callback that should be triggered
-  * after modal has been rendered.
-  *
-  * @example
-  * ioco.modal('close')
-  * closes the modal.
-  */
-  function modal( html, options ){
-
-    function closeModal(){
-      if( options && typeof(options.on) === 'function' )
-        options.on('close', $('#ioco-modal') );
-      $('.ioco-modal').fadeOut(300);
-      setTimeout( function(){
-        $('.ioco-modal').remove();
-      }, 300);
-      $(window).off( 'resize', checkModalHeight );
-    }
-
-    function checkModalHeight(){
-      if( $('#ioco-modal').height() > $(window).height() - 40 )
-        $('#ioco-modal').animate({ height: $(window).height() - 40 }, 200);
-      else
-        $('#ioco-modal').animate({ height: $('#ioco-modal').data('origHeight') }, 200);
-    }
-
-    function setupModalActions(){
-      if( $('#ioco-modal .modal-sidebar').length > 0 ){
-        $('#ioco-modal .modal-sidebar > .sidebar-nav li').on('click', function(){
-          $(this).closest('ul').find('.active').removeClass('active');
-          $('#ioco-modal .sidebar-content > div').hide();
-          $($('#ioco-modal .sidebar-content > div')[$(this).index()]).show();
-          $(this).addClass('active');
-        }).first().click();
-      }
-      if( options && options.completed && typeof(options.completed) === 'function' )
-        setTimeout(function(){ options.completed( $('#ioco-modal') ); }, 500 );
-    }
-
-    if( html === 'close' )
-      return closeModal();
-    else if( typeof(html) === 'object' ){
-      options = html;
-      html = null;
-    }
-
-    $('.ioco-modal').remove();
-    $('body').append('<div id="ioco-modal-overlay" class="ioco-modal"/>')
-      .append('<div id="ioco-modal" class="ioco-modal"><div class="modal-inner-wrapper" /></div>');
-    var closeModalBtn = $('<a class="close-icn">&times;</a>');
-    $('#ioco-modal').prepend(closeModalBtn);
-    if( options.windowControls ){
-      var countWinCtrlBtns = 1;
-      for( ctrl in options.windowControls ){
-        var winCtrlBtn = $('<a winCtrl="'+ctrl+'" class="modal-win-ctrl live-tipsy" href="#" original-title="'+options.windowControls[ctrl].title+'"><span class="icn '+options.windowControls[ctrl].icn+'" /></a>');
-        winCtrlBtn.css( { right: 16*(countWinCtrlBtns++)+32 } );
-        $('#ioco-modal').prepend(winCtrlBtn);
-        winCtrlBtn.on('click', function(e){
-          e.preventDefault();
-          options.windowControls[$(this).attr('winCtrl')].callback( $('#ioco-modal') );
-        })
-      }
-    }
-    closeModalBtn.on('click', closeModal);
-    $('#ioco-modal-overlay').fadeIn(200).on('click', closeModal);
-    if( options && options.title )
-      $('#ioco-modal').prepend('<span class="modal-title">'+options.title+'</span>');
-
-
-    // height configuration
-    if( options && options.height && typeof(options.height) === 'number' )
-      $('#ioco-modal').css( 'height', options.height );
-    $('#ioco-modal').data('origHeight', $('#ioco-modal').height());
-
-    checkModalHeight();
-    $(window).on( 'resize', checkModalHeight );
-
-    if( options.url ){
-      $('#ioco-modal .modal-inner-wrapper').load( options.url, function(){
-        if( options && options.before && typeof(options.before) === 'function' )
-          options.before( $('#ioco-modal') );
-        $('#ioco-modal').fadeIn( 200 );
-        setupModalActions();
-      });
-    } else {
-      html = html || options.data || options.html;
-      if( options.$content )
-        $('#ioco-modal .modal-inner-wrapper').append( options.$content );
-      else if( html )
-        $('#ioco-modal .modal-inner-wrapper').html( html ).fadeIn(200);
-
-      if( options && options.before && typeof(options.before) === 'function' )
-        options.before( $('#ioco-modal') );
-      setupModalActions();
-    }
-
-  };
 
   /**
    * add a notification message to the
@@ -290,61 +95,43 @@
     });
   };
 
-  /**
-   * translates doms with .translation class
-   * to loaded i18n
-   */
-  function translate( $obj ){
-    if( $obj.hasClass('translate') ){
-      if( $obj.attr('data-ns') )
-        $obj.html( $.i18n.t( $obj.attr('data-ns') + '.' + $obj.text() ) );
-      else
-        $obj.html( $.i18n.t( $obj.text() ) );
-      $obj.removeClass('translate');
-    }
-  }
-
-  function parseTranslations(){
-    $('.translate').each( function(){
-      translate( $(this) );
-    });
-  }
-
-  /**
-   * prompt for user input
-   */
-  function prompt( title, $content, options ){
-    ioco.modal({
-      title: title,
-      height: 200,
-      width: 300,
-      $content: $content,
-      completed: function( $modal ){
-        $modal.find('input[type=text]:first').focus();
-        $modal.find('form').on('submit', function(e){
-          if( typeof(options.onSubmit) === 'function' ){
-            e.preventDefault();
-            options.onSubmit( $modal, e );
-          }
-        });
-      }
-    });
-  }
-
-
   var root = this; // window of browser
 
   if( !root.ioco || typeof( root.ioco ) !== 'object' )
     root.ioco = {};
   root.ioco.notify = notify;
-  root.ioco.modal = modal;
-  root.ioco.prompt = prompt;
   root.ioco.loaderHtml = loader;
-  root.ioco.translate = translate;
-  root.ioco.window = window;
-  root.ioco.parseTranslations = parseTranslations;
+  root.ioco.prompt = prompt;
   root.ioco.require = require;
   root.ioco.sources = sources;
   root.ioco.log = log;
 
+  $(document).ready(function(){
+    root.ioco._csrf = $('#_csrf').val();
+  });
+
+
 })();
+
+// Avoid "console" errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+      'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+      'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+      'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+      'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+      method = methods[length];
+
+      // Only stub undefined methods.
+      if (!console[method]) {
+          console[method] = noop;
+      }
+    }
+}());
